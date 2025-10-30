e00ff4dc0f6e6a21a37ba8593bf8baeffb99098e
Ludwig Schmauder <ludwig.schmauder@uni-ulm.de>
2025-10-30 16:29:41 +0100

    Delete MA2-main directory

--- MA2-main/tabletop/bridge/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Integration helpers for external hardware bridges."""
-
-from .neon import NeonEyeTrackerBridge, load_neon_configuration
-
-__all__ = ["NeonEyeTrackerBridge", "load_neon_configuration"]
